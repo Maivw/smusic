665ee714753624c340ffc4dd34cdb0fc3f741606
tran <fidtran@gmail.com>
2021-04-17 17:23:11 -0400

    styled cardSong component

--- src/component/cardSong/cardSong.js
+++ src/component/cardSong/cardSong.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./cardSong.css";
+import { Link } from "react-router-dom";
 
 function CardSong({ album }) {
 	console.log("aaa", album);
 	return (
-		<div>
-			<div className="cardSong">
-				<div className="cardSong__imgWrapper">
-					<img
-						className="cardSong__img"
-						src={album?.imageUri}
-						alt="album image"
-					/>
+		<Link to="/album/:1" className="cardSong__link">
+			<div>
+				<div className="cardSong">
+					<div className="cardSong__imgWrapper">
+						<img
+							className="cardSong__img"
+							src={album?.imageUri}
+							alt="album image"
+						/>
+					</div>
+					<div className="cardSong__title">{album?.title}</div>
 				</div>
-				<div className="cardSong__title">{album?.title}</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
 
