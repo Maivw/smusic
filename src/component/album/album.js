7971e83c1cbcaa5dedde115fb473525a407059bc
tran <fidtran@gmail.com>
2021-04-17 15:59:58 -0400

    changed header compnent, worked on album screen UI, added songs data file to data directory

--- src/component/album/album.js
+++ src/component/album/album.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Navbar from "../navbar/navbar";
+import Header from "../header/header";
+import AlbumBody from "../albumBody/albumBody";
+import AlbumFooter from "../albumFooter/albumFooter";
+import "./album.css";
 
 function Album() {
 	return (
 		<div className="album">
-			<div className="album">hihi</div>
+			<Header />
+			<div className="album__header"></div>
+			<div className="album__body-left">
+				<Navbar />
+			</div>
+			<div className="album__body-right">
+				<AlbumBody />
+			</div>
+
+			<div className="album__footer">
+				<AlbumFooter />
+			</div>
 		</div>
 	);
 }
